3e54c9968a4fc477476866211ec9cb6bd82de8b5
Consultant <consultant@Admins-MacBook-Pro-5.local>
2022-08-29 09:46:49 -0400

    added dependency

--- app/src/main/java/com/example/googlebooksapi/util/BookNavigation.kt
+++ app/src/main/java/com/example/googlebooksapi/util/BookNavigation.kt
@@ -1,2 +1,7 @@
 package com.example.googlebooksapi.util
 
+const val SEARCH_SCREEN = "book/search"
+const val DETAIL_SCREEN = "book/search/details/"
+const val ARG_BOOK = "book"
+var navigateDetailScreen = "$DETAIL_SCREEN{$ARG_BOOK}"
+
